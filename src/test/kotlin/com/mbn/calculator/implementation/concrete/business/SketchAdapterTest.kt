03caeb56980a79ac21ff5b62cfc907bda929085c
Mario Beani <mariobeanineto@gmail.com>
2024-03-19 21:02:52 -0300

    adding exceptions flow

---
+++ src/test/kotlin/com/mbn/calculator/implementation/concrete/business/SketchAdapterTest.kt
@@ -0,0 +1,10 @@
+package com.mbn.calculator.implementation.concrete.business
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class SketchAdapterTest {
+
+}
